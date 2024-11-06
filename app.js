f22589a5a5c8aff498856f75039cef11eac98c0c
mutiara126 <salsamutiara1812@gmail.com>
2024-11-06 20:08:00 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const todoRoutes = require('./routes/tododb.js');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
