832bb112a84fec8238ae6c0c7b8f81a3647e940d
mutiara126 <salsamutiara1812@gmail.com>
2024-11-20 21:22:51 +0700

    Middleware

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require('express');
-const todoRoutes = require('./routes/tododb.js');
+const todoRoutes = require('./routes/tododb.js')
 const app = express();
+
 require('dotenv').config();
 const port = process.env.PORT;
 
+const expressLayout = require('express-ejs-layouts')
+
+const db = require('./database/db')
+
+app.use(expressLayout);
 
 app.use(express.json());
 
 app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', (req,res) => {
+    res.render('index',{
+        layout: 'layouts/main-layout'
+    });
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
+app.get('/contact', (req,res) => {
+    res.render('contact', {
+        layout: 'layouts/main-layout'
+    });
 });
 
-app.use((req, res) => {
-    res.status(404).send('404 - Page Not Found');
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
-app.listen(port, () => {
+app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}/`);
 });
